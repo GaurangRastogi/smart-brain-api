05680e6d1d922447fa6740f18b1a57b06e7ad8e7
Gaurang <rastogigaurang209@gmail.com>
2022-08-04 15:57:11 +0530

    adding database

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const register=require('./controller/register');
 const signin = require('./controller/signin');
 const profile=require('./controller/profile');
 const image= require('./controller/image');
-
+process.env.NODE_TLS_REJECT_UNAUTHORIZED=0
 const db=knex({
     client: 'pg',
     connection: {
